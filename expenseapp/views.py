5d085752dcbbe6e9a28b112796d87491daa9be2c
yashsindhu04 <yashsindhu4903@gmail.com>
2023-11-01 20:01:26 +0530

    Design and Infographics update

--- expenseapp/views.py
+++ expenseapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect,get_object_or_404
+from django.db.models import Sum
 
 # Create your views here.
 from .forms import ExpenseForm
@@ -18,7 +19,8 @@ def add_expense(request):
 
 def expense_list(request):
     expenses = Expense.objects.all()
-    return render(request, 'expense_list.html', {'expenses': expenses})
+    total_expense = Expense.objects.aggregate(total=Sum('amount'))['total'] or 0
+    return render(request, 'expense_list.html', {'expenses': expenses, 'total_expense': total_expense})
 
 
 def edit_expense(request, expense_id):
@@ -42,7 +44,29 @@ def delete_expense(request, expense_id):
         expense.delete()
         return redirect('expenseapp:expense_list')
 
-    return render(request, 'delete_expense_view.html', {'expense': expense})
+    return render(request, 'delete_expense_view.html', {'expense': expense})  
+
+import matplotlib.pyplot as plt
+import io
+import base64
+
+def infographics(request):
+    # Get the data for your categorical spending chart
+    spending = Expense.objects.values('category').annotate(total=Sum('amount'))
     
-        
-        
+    category_labels = [item['category'] for item in spending]
+    category_values = [item['total'] for item in spending]
+
+    # Create the categorical spending chart
+    plt.figure(figsize=(8, 6))
+    plt.pie(category_values, labels=category_labels, autopct='%1.1f%%', startangle=140)
+    plt.title('Categorical Spending Chart')
+
+    # Save the chart as an image
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    chart_image = base64.b64encode(buffer.read()).decode()
+    buffer.close()
+
+    return render(request, 'infographics.html', {'chart_image': chart_image})
